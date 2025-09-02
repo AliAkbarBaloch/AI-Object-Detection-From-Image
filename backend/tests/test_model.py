2114b2c4f54525614b2548a0273708c1e0c34ae9
Ali Akbar <aliakbar@Alis-MacBook-Pro.local>
2025-09-02 17:54:27 +0200

    Converting Image into float32

--- backend/tests/test_model.py
+++ backend/tests/test_model.py
@@ -1,11 +1,14 @@
 import unittest
 from models.model import count_objects
 import os
+import numpy as np
 
 class ModelPipelineTestCase(unittest.TestCase):
     def test_count_objects(self):
         test_image_path = os.path.join(os.path.dirname(__file__), '../uploads/image.png')
-        result = count_objects(test_image_path, 'car')
+        image_np = np.array(Image.open(test_image_path)).astype(np.float32)
+        # result = count_objects(test_image_path, 'car')
+        result = count_objects(image_np, 'car')
         self.assertIn('count', result)
         self.assertIn('labels', result)
         self.assertIn('segments', result)
