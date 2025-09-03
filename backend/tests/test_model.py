874067497550c1ec0ad55a742e261863c78bae25
Ali Akbar <aliakbar@Alis-MacBook-Pro.local>
2025-09-03 15:10:54 +0200

    Modifying Coveragerc and Model Test Cases

--- backend/tests/test_model.py
+++ backend/tests/test_model.py
@@ -2,7 +2,9 @@ import unittest
 from models.model import count_objects
 import os
 from models.db import client
+import os
 
+@unittest.skipIf(os.getenv("CI") == "true", "Skip heavy model pipeline test in CI environment")
 class ModelPipelineTestCase(unittest.TestCase):
     def test_count_objects(self):
         test_image_path = os.path.join(os.path.dirname(__file__), '../uploads/image.png')
